b0897552b08396cca5411f19b75c8be556d750aa
Pedro Carbajal <pedro_c@beezwax.net>
2021-10-01 12:01:47 +0900

    Bump version to 0.3.0.1

--- lib/aws/cognito_srp/version.rb
+++ lib/aws/cognito_srp/version.rb
@@ -2,6 +2,9 @@
 
 module Aws
   class CognitoSrp
-    VERSION = "0.3.0"
+    VERSION = "0.3.0.1"
+    #                ^
+    # NOTE: the fourth version number is just temporary to reflect an update to
+    # the gemspec (with no functional changes), remove it in next version bump
   end
 end
